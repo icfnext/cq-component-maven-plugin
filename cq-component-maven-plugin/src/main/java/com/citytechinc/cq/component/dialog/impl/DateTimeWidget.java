0b45760e3b3d91e6a05c4296dd0b0c6351cdb97d
Michael Hodgdon <mhodgdon@citytechinc.com>
2013-04-01 16:01:36 -0500

    added datetime

---
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/impl/DateTimeWidget.java
@@ -0,0 +1,19 @@
+package com.citytechinc.cq.component.dialog.impl;
+
+import java.util.Map;
+
+import com.citytechinc.cq.component.annotations.config.Widget;
+import com.citytechinc.cq.component.dialog.AbstractWidget;
+
+@Widget(annotationClass = "com.citytechinc.cq.component.annotations.widgets.DateTime", makerClass = "com.citytechinc.cq.component.dialog.maker.impl.DateTimeWidgetMaker", xtypes = DateTimeWidget.XTYPE)
+public class DateTimeWidget extends AbstractWidget {
+	public static final String XTYPE = "datetime";
+	private static final String PRIMARY_TYPE = "cq:Widget";
+
+	public DateTimeWidget(String fieldLabel, String fieldDescription,
+			boolean allowBlank, String defaultValue, String name,
+			String fieldName, Map<String, String> additionalProperties) {
+		super(XTYPE, fieldLabel, fieldDescription, allowBlank, defaultValue,
+				name, PRIMARY_TYPE, null, fieldName, additionalProperties, null);
+	}
+}
---
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/maker/impl/DateTimeWidgetMaker.java
@@ -0,0 +1,40 @@
+package com.citytechinc.cq.component.dialog.maker.impl;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+import javassist.CannotCompileException;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.NotFoundException;
+
+import com.citytechinc.cq.component.annotations.DialogField;
+import com.citytechinc.cq.component.dialog.DialogElement;
+import com.citytechinc.cq.component.dialog.exception.InvalidComponentFieldException;
+import com.citytechinc.cq.component.dialog.impl.DateTimeWidget;
+import com.citytechinc.cq.component.dialog.maker.AbstractWidgetMaker;
+
+public class DateTimeWidgetMaker extends AbstractWidgetMaker{
+
+	@Override
+	public DialogElement make(String xtype, Field widgetField,
+			CtField ctWidgetField, Class<?> containingClass,
+			CtClass ctContainingClass, Map<Class<?>, String> xtypeMap)
+			throws ClassNotFoundException, InvalidComponentFieldException,
+			CannotCompileException, NotFoundException {
+	
+		DialogField dialogFieldAnnotation = (DialogField) ctWidgetField.getAnnotation(DialogField.class);
+		
+		String name = getNameForField(dialogFieldAnnotation, widgetField);
+		String fieldName = getFieldNameForField(dialogFieldAnnotation, widgetField);
+		String fieldLabel = getFieldLabelForField(dialogFieldAnnotation, widgetField);
+		String fieldDescription = getFieldDescriptionForField(dialogFieldAnnotation);
+		Boolean isRequired = getIsRequiredForField(dialogFieldAnnotation);
+		Map<String, String> additionalProperties = getAdditionalPropertiesForField(dialogFieldAnnotation);
+		String defaultValue = getDefaultValueForField(dialogFieldAnnotation);
+		
+		
+		return new DateTimeWidget(fieldLabel, fieldDescription, !isRequired, defaultValue, name, fieldName, additionalProperties);
+	}
+
+}

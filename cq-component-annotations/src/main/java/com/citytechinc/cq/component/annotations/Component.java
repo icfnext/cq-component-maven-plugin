a92dde659befd243ed3d470c02d2e14aa12bc5c8
Paul Michelotti <pmichelotti@citytechinc.com>
2013-07-27 19:27:59 -0500

    Updates to JavaDocs for Component annotation

--- cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/Component.java
+++ cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/Component.java
@@ -10,11 +10,10 @@ import com.citytechinc.cq.component.annotations.editconfig.DropTarget;
 import com.citytechinc.cq.component.annotations.editconfig.FormParameter;
 
 /**
- * The Component marks your class as a CQ Component. Tools will use this
- * annotation to determine whether to perform an operation on the class or not.
- * 
- * 
- * 
+ * The Component annotation is used to indicate that a Class represents a CQ
+ * Component.  Multiple properties which are universally applicable to all types of
+ * components are exposed for configuration via this annotation.
+ *
  */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
@@ -22,8 +21,8 @@ public @interface Component {
 
 	/**
 	 * Overrides the baseComponentPath
-	 * 
-	 * 
+	 *
+	 *
 	 * @return String
 	 */
 	String basePath() default "";
@@ -32,196 +31,283 @@ public @interface Component {
 	 * The path to the component definition within the context of a CQ content
 	 * tree. This is meant to override any default path specified for the
 	 * project as a whole.
-	 * 
-	 * 
+	 *
+	 *
 	 * @return String
 	 */
 	String path() default "";
 
 	/**
-	 * The name which will identify this component. For output purposes this is
-	 * the directory name under which the configuration files will be placed.
-	 * 
-	 * 
+	 * The name which will identify this component. This property should be used
+	 * if the name which will be automatically generated for the component based on
+	 * the name of the component class is not appropriate for your purposes.
+	 *
 	 * @return String
 	 */
 	String name() default "";
 
 	/**
-	 * The jcr:title of the Component
-	 * 
+	 * Populates the jcr:title of the Component's .content.xml
+	 *
 	 * @return String
 	 */
 	String value();
 
 	/**
-	 * Populates the jcr:description property of .content.xml
-	 * 
+	 * Populates the jcr:description property of the Component's .content.xml
+	 *
 	 * @return String
 	 */
 	String description() default "";
 
 	/**
 	 * The component group into which this component will be placed. This
-	 * overrides any default group established elsewhere.
-	 * 
-	 * 
+	 * overrides any default group established for your project's build.
+	 *
 	 * @return String
 	 */
 	String group() default "";
 
 	/**
 	 * Indication of whether this component is a container for other components.
-	 * Populates the cq:isContainer property of .content.xml.
-	 * 
+	 * Populates the cq:isContainer property of the Component's .content.xml.
+	 *
 	 * @return boolean
 	 */
 	boolean isContainer() default false;
 
 	/**
 	 * Indication of whether the component should not be rendered within a
-	 * container. Populates the cq:noDecoration property of .content.xml.
-	 * 
+	 * container. Populates the cq:noDecoration property of the Component's .content.xml.
+	 *
 	 * @return boolean
 	 */
 	boolean noDecoration() default false;
 
 	/**
-	 * Populates the cq:templatePath property of .content.xml.
-	 * 
+	 * Populates the cq:templatePath property of the Component's .content.xml.
+	 *
 	 * @return String
 	 */
 	String templatePath() default "";
 
 	/**
-	 * Populates the dialogPath property of .content.xml.
-	 * 
+	 * Populates the dialogPath property of the Component's .content.xml.
+	 *
 	 * @return String
 	 */
 	String dialogPath() default "";
 
 	/**
-	 * Populates the jcr:created property of .content.xml.
-	 * 
+	 * Populates the jcr:created property of the Component's .content.xml.
+	 *
 	 * @return String
 	 */
 	String created() default "";
 
 	/**
-	 * An array to order the tabs used in DialogField annotations
-	 * 
+	 * Establishes the set of Tabs which will appear within a Component's authoring dialog.
+	 * When a multi-tab dialog is needed, all tabs must be defined via this property.  Individual
+	 * dialog fields indicate which tab they are to be placed into by number, starting from '1'.
+	 * For more detail concerning placing Dialog Fields into tabs, see
+	 * {@link com.citytechinc.cq.component.annotations.DialogField#tab()}
+	 *
 	 * @return Tab[]
 	 */
 	Tab[] tabs() default {};
 
 	/**
-	 * The cq:actions for the edit config
-	 * 
+	 * The list of actions which will populate the cq:actions attribute of the _cq_editConfig.xml.
+	 * For more information on valid actions see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:actions">Configuring Edit Behavior Documentation</a>
+	 *
 	 * @return String[]
 	 */
 	String[] actions() default {};
 
 	/**
-	 * The cq:dialogMode for the edit config
-	 * 
+	 * Populates the cq:dialogMode property of the _cq_editConfig.xml.  For more information on
+	 * valid dialog modes see the <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:dialogMode">Configuring Edit Behavior Documentation</a>
+	 *
 	 * @return String
 	 */
 	String dialogMode() default "floating";
 
 	/**
-	 * The cq:layout for the edit config
-	 * 
+	 * Populates the cq:layout property of the _cq_editConfig.xml.  For more information on
+	 * valid layouts see the <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:layout">Configuring Edit Behavior Documentation</a>
+	 *
 	 * @return String
 	 */
 	String layout() default "editbar";
 
 	/**
-	 * Listeners inside the edit config
-	 * 
+	 * Establishes the set of edit listeners which will be associated with the Component.
+	 * These populate the cq:listeners node of the _cq_editConfig.xml.  For more information
+	 * on the purpose of and configuration for listeners, see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:listeners">Configuring Edit Behavior Documentation</a>
+	 *
 	 * @return Listener[]
 	 */
 	Listener[] listeners() default {};
 
 	/**
-	 * The sling:resourceSuperType of this component
-	 * 
+	 * Establishes the sling:resourceSuperType of this component
+	 *
 	 * @return String
 	 */
 	String resourceSuperType() default "";
 
+	/**
+	 * Establishes the empty text presented for components which are drop targets.
+	 *
+	 * @return String
+	 */
 	String emptyText() default "Drag components or assets here";
 
+	/**
+	 * Indicates whether this component should inherit edit behavior from it's super type.
+	 * Populates the cq:inherit property of the _cq_editConfig.xml.  For more information on
+	 * inheriting edit behavior configuration see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:inherit">Configuring Edit Behavior Documentation</a>
+	 *
+	 * @return boolean
+	 */
 	boolean editConfigInherit() default false;
 
 	/**
-	 * The name of the xml file to store this dialog under (with out the .xml
-	 * suffix)
-	 * 
+	 * The name, without extension, to use for the output dialog xml file.  This can be used
+	 * when you want to output a dialog type other than the edit dialog.  For example, a design dialog
+	 * may be written for a component by setting the fileName to 'design_dialog'.
+	 *
 	 * @return String
 	 */
 	String fileName() default "dialog";
 
 	/**
-	 * boolean setting to generate an edit config
-	 * 
+	 * Indicates whether a _cq_editConfig file should be generated for the component defined by the
+	 * annotated Java Class.  When set to false, a _cq_editConfig file will not be generated regardless of
+	 * whether one already exists for the Component.  When set to true, a _cq_editConfig file will only
+	 * be generated if one does not already exist for the Component.
+	 *
 	 * @return boolean
 	 */
 	boolean editConfig() default true;
 
 	/**
-	 * The width of the dialog
-	 * 
+	 * The width of the dialog popup widget.  The default value -1 indicates that the default width should be used.
+	 *
 	 * @return int
 	 */
 	int dialogWidth() default -1;
 
 	/**
-	 * The height of the dialog
-	 * 
+	 * The height of the dialog popup widget.  The default value -1 indicates that the default height should be used.
+	 *
 	 * @return int
 	 */
 	int dialogHeight() default -1;
 
 	/**
-	 * The allowedChildren property of .content.xml
-	 * 
+	 * Establishes the components which are allowed to be children of this component.
+	 * This list populates the allowedChildren property of .content.xml.  For more information
+	 * on the usage of allowedChildren see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components.html#Properties and Child Nodes of a Component">Component Documentation</a>.
+	 *
 	 * @return String[]
 	 */
 	String[] allowedChildren() default {};
 
 	/**
-	 * The allowedParents property of .content.xml
-	 * 
+     * Establishes the components which are allowed to be parents of this component.
+     * This list populates the allowedParents property of .content.xml.  For more information
+     * on the usage of allowedParents see the
+     * <a href="http://dev.day.com/docs/en/cq/current/developing/components.html#Properties and Child Nodes of a Component">Component Documentation</a>.
+     *
 	 * @return String[]
 	 */
 	String[] allowedParents() default {};
 
 	/**
-	 * The cq:cellName property of .content.xml
-	 * 
+	 * Populates the cq:cellName property of the .content.xml.  For more information on the intended
+	 * usage of the cq:cellName property, see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components.html#Properties and Child Nodes of a Component">Component Documentation</a>.
+	 *
 	 * @return String
 	 */
 	String cellName() default "";
 
 	/**
-	 * An array of ActionConfig's for the edit config file
-	 * 
+	 * Establishes the set of Action Configurations which will be written to the cq:actionConfigs node
+	 * of the _cq_editConfig.xml.  For more information on Action Configurations see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:actionConfigs">Configuring Edit Behavior Documentation</a>.
+	 *
 	 * @return ActionConfig[]
 	 */
 	ActionConfig[] actionConfigs() default {};
 
+	/**
+	 * Indicates whether in place editing should be active for this component.  This property populates
+	 * the active property of the cq:inplaceEditing node in the _cq_editConfig.xml.  For more information on
+	 * inplace editing, see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:inplaceEditing">Configuring Edit Behavior Documentation</a>.
+	 *
+	 * @return boolean
+	 */
 	boolean inPlaceEditingActive() default true;
 
+	/**
+     * Establishes the path to the inplace editing configuration.  This property populates
+     * the configPath property of the cq:inplaceEditing node in the _cq_editConfig.xml.  For more information on
+     * inplace editing, see the
+     * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:inplaceEditing">Configuring Edit Behavior Documentation</a>.
+     *
+     * @return String
+     */
 	String inPlaceEditingConfigPath() default "";
 
+	/**
+     * Establishes the type of inplace editor which will be used for the annotated component.  This property populates
+     * the editorType property of the cq:inplaceEditing node in the _cq_editConfig.xml.  For more information on
+     * inplace editing, see the
+     * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:inplaceEditing">Configuring Edit Behavior Documentation</a>.
+     *
+     * @return String
+     */
 	String inPlaceEditingEditorType() default "";
 
+	/**
+	 * Establishes the set of form parameters written to the cq:formParameters node of the _cq_editConfig.xml.
+	 * For more information on form parameters see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:formParameters">Configuring Edit Behavior Documentation</a>.
+	 *
+	 * @return String
+	 */
 	FormParameter[] formParameters() default {};
 
+	/**
+	 * Establishes the list of drop targets written to the cq:dropTargets node of the _cq_editConfig.xml.
+	 * For more information on drop targets and their usage, see the
+	 * <a href="http://dev.day.com/docs/en/cq/current/developing/components/edit_config.html#cq:dropTargets">Configuring Edit Behavior Documentation</a>.
+	 *
+	 * @return DropTarget[]
+	 */
 	DropTarget[] dropTargets() default {};
 
+	/**
+	 *
+	 * @return ContentProperty[]
+	 */
 	ContentProperty[] contentAdditionalProperties() default {};
 
+	/**
+	 * Indicates whether the Target context menu should be disabled for the particular component.
+	 * Populates the cq:disableTargeting property of the _cq_editConfig.xml.
+	 * For information on this properties usage, see
+	 * <a href="http://forums.adobe.com/message/5334713">this forum article</a>.
+	 *
+	 * @return boolean
+	 */
 	boolean disableTargeting() default false;
 
 }

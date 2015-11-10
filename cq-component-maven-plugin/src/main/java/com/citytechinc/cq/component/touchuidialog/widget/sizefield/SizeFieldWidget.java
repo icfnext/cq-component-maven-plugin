51f04a86c7e6576815ec9ba85361093ec912a3c4
pmichelotti <pmichelotti@citytechinc.com>
2015-11-09 22:34:45 -0500

    Initial size field implementation

---
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/widget/sizefield/SizeFieldWidget.java
@@ -0,0 +1,32 @@
+/**
+ *    Copyright 2013 CITYTECH, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.citytechinc.cq.component.touchuidialog.widget.sizefield;
+
+import com.citytechinc.cq.component.annotations.config.TouchUIWidget;
+import com.citytechinc.cq.component.annotations.widgets.SizeField;
+import com.citytechinc.cq.component.touchuidialog.widget.AbstractTouchUIWidget;
+
+@TouchUIWidget(annotationClass = SizeField.class, makerClass = SizeFieldWidgetMaker.class,
+        resourceType = SizeFieldWidget.RESOURCE_TYPE)
+public class SizeFieldWidget extends AbstractTouchUIWidget {
+
+    public static final String RESOURCE_TYPE = "cq/gui/components/authoring/dialog/sizefield";
+
+    public SizeFieldWidget(SizeFieldWidgetParameters parameters) {
+        super(parameters);
+    }
+
+}
---
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/widget/sizefield/SizeFieldWidgetMaker.java
@@ -0,0 +1,41 @@
+/**
+ *    Copyright 2013 CITYTECH, Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.citytechinc.cq.component.touchuidialog.widget.sizefield;
+
+import com.citytechinc.cq.component.dialog.exception.InvalidComponentFieldException;
+import com.citytechinc.cq.component.touchuidialog.TouchUIDialogElement;
+import com.citytechinc.cq.component.touchuidialog.exceptions.TouchUIDialogGenerationException;
+import com.citytechinc.cq.component.touchuidialog.widget.maker.AbstractTouchUIWidgetMaker;
+import com.citytechinc.cq.component.touchuidialog.widget.maker.TouchUIWidgetMakerParameters;
+import com.citytechinc.cq.component.touchuidialog.widget.numberfield.NumberFieldWidget;
+import com.citytechinc.cq.component.touchuidialog.widget.numberfield.NumberFieldWidgetParameters;
+
+public class SizeFieldWidgetMaker extends AbstractTouchUIWidgetMaker<SizeFieldWidgetParameters> {
+
+    public SizeFieldWidgetMaker(TouchUIWidgetMakerParameters parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected TouchUIDialogElement make(SizeFieldWidgetParameters parameters) throws ClassNotFoundException, InvalidComponentFieldException, TouchUIDialogGenerationException, IllegalAccessException, InstantiationException {
+
+        parameters.setHeight(new NumberFieldWidget(new NumberFieldWidgetParameters()));
+        parameters.setWidth(new NumberFieldWidget(new NumberFieldWidgetParameters()));
+
+        return new SizeFieldWidget(parameters);
+    }
+
+}

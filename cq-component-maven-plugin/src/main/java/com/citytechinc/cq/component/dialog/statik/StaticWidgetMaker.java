ee4c99f3a3143acecf14832c3679ee2e4bfd6324
Christopher Leggett <christopher.leggett@icfi.com>
2015-11-13 11:07:52 -0500

    Update static widget with latest changes.

--- cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/statik/StaticWidgetMaker.java
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/dialog/statik/StaticWidgetMaker.java
@@ -1,17 +1,17 @@
 /**
- *    Copyright 2013 CITYTECH, Inc.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2013 CITYTECH, Inc.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.citytechinc.cq.component.dialog.statik;
 
@@ -21,42 +21,27 @@ import com.citytechinc.cq.component.dialog.maker.AbstractWidgetMaker;
 import com.citytechinc.cq.component.dialog.maker.WidgetMakerParameters;
 import com.google.common.base.Strings;
 
-public class StaticWidgetMaker extends AbstractWidgetMaker {
-	public StaticWidgetMaker(final WidgetMakerParameters parameters) {
-		super(parameters);
-	}
+public class StaticWidgetMaker extends AbstractWidgetMaker<StaticWidgetParameters> {
+    public StaticWidgetMaker(final WidgetMakerParameters parameters) {
+        super(parameters);
+    }
 
-	@Override
-	public final DialogElement make() throws ClassNotFoundException {
-		final Static staticAnnotation = getAnnotation(Static.class);
-		final StaticWidgetParameters staticWidgetParameters = buildStaticWidgetParameters(staticAnnotation);
-		return new StaticWidget(staticWidgetParameters);
-	}
+    @Override
+    public final DialogElement make(final StaticWidgetParameters parameters) throws ClassNotFoundException {
+        final Static staticAnnotation = getAnnotation(Static.class);
 
-	private StaticWidgetParameters buildStaticWidgetParameters(final Static staticAnnotation) {
-		final StaticWidgetParameters staticWidgetParameters = new StaticWidgetParameters();
+        parameters.setBold(staticAnnotation.bold());
+        parameters.setBottommargin(staticAnnotation.bottommargin());
+        parameters.setHref(Strings.emptyToNull(staticAnnotation.href()));
+        parameters.setHtml(Strings.emptyToNull(staticAnnotation.html()));
+        parameters.setItalic(staticAnnotation.italic());
+        parameters.setNoWrap(staticAnnotation.noWrap());
+        parameters.setSmall(staticAnnotation.small());
+        parameters.setTag(Strings.emptyToNull(staticAnnotation.tag()));
+        parameters.setTarget(Strings.emptyToNull(staticAnnotation.target()));
+        parameters.setText(Strings.emptyToNull(staticAnnotation.text()));
+        parameters.setTopmargin(staticAnnotation.topmargin());
 
-		//Set custom widget parameters
-		staticWidgetParameters.setBold(staticAnnotation.bold());
-		staticWidgetParameters.setBottommargin(staticAnnotation.bottommargin());
-		staticWidgetParameters.setHref(Strings.emptyToNull(staticAnnotation.href()));
-		staticWidgetParameters.setHtml(Strings.emptyToNull(staticAnnotation.html()));
-		staticWidgetParameters.setItalic(staticAnnotation.italic());
-		staticWidgetParameters.setNoWrap(staticAnnotation.noWrap());
-		staticWidgetParameters.setSmall(staticAnnotation.small());
-		staticWidgetParameters.setTag(Strings.emptyToNull(staticAnnotation.tag()));
-		staticWidgetParameters.setTarget(Strings.emptyToNull(staticAnnotation.target()));
-		staticWidgetParameters.setText(Strings.emptyToNull(staticAnnotation.text()));
-		staticWidgetParameters.setTopmargin(staticAnnotation.topmargin());
-
-		//Set parameters from DialogField
-		staticWidgetParameters.setFieldLabel(getFieldLabelForField());
-		staticWidgetParameters.setFieldName(getFieldNameForField());
-		staticWidgetParameters.setFieldDescription(getFieldDescriptionForField());
-		staticWidgetParameters.setHideLabel(getHideLabelForField());
-		staticWidgetParameters.setAdditionalProperties(getAdditionalPropertiesForField());
-		staticWidgetParameters.setListeners(getListeners());
-
-		return staticWidgetParameters;
-	}
+        return new StaticWidget(parameters);
+    }
 }

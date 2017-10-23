8e15df5a82b086db4822994aee4b2e5176e6df62
Mark Daugherty <mark.r.daugherty@gmail.com>
2017-10-23 10:29:42 -0500

    added 'allowUpload' parameter for touch UI image widget.

--- cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/widget/fileupload/FileUploadWidget.java
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/widget/fileupload/FileUploadWidget.java
@@ -31,6 +31,7 @@ public class FileUploadWidget extends AbstractTouchUIWidget {
 	private final String text;
 	private final String icon;
 	private final boolean multiple;
+	private final boolean allowUpload;
 	private final String uploadUrl;
 	private final String uploadUrlBuilder;
 	private final Long sizeLimit;
@@ -46,6 +47,7 @@ public class FileUploadWidget extends AbstractTouchUIWidget {
 		text = parameters.getText();
 		icon = parameters.getIcon();
 		multiple = parameters.isMultiple();
+		allowUpload = parameters.isAllowUpload();
 		uploadUrl = parameters.getUploadUrl();
 		uploadUrlBuilder = parameters.getUploadUrlBuilder();
 		sizeLimit = parameters.getSizeLimit();
@@ -66,6 +68,10 @@ public class FileUploadWidget extends AbstractTouchUIWidget {
 
 	public String getIcon() {
 		return icon;
+	}
+
+	public boolean isAllowUpload() {
+		return allowUpload;
 	}
 
 	public boolean isMultiple() {

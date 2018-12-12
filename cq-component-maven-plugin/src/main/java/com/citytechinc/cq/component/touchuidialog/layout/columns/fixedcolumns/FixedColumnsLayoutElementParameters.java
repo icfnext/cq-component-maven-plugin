624d606a2b9be4aa78fc55603fad43ea1926651f
Mark Daugherty <mark.r.daugherty@gmail.com>
2018-12-12 12:13:04 -0600

    field name default value.

--- cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/layout/columns/fixedcolumns/FixedColumnsLayoutElementParameters.java
+++ cq-component-maven-plugin/src/main/java/com/citytechinc/cq/component/touchuidialog/layout/columns/fixedcolumns/FixedColumnsLayoutElementParameters.java
@@ -8,83 +8,88 @@ import org.codehaus.plexus.util.StringUtils;
 
 public class FixedColumnsLayoutElementParameters extends LayoutElementParameters {
 
-	private String title;
-	private String path;
-	private TouchUIDialogElement renderCondition;
-	private boolean showOnCreate;
-	private boolean hideOnEdit;
-	private String orderBefore;
+    private String title;
 
-	@Override
-	public String getResourceType() {
-		if (StringUtils.isNotEmpty(path)) {
-			return Section.INCLUDE_RESOURCE_TYPE;
-		} else if(TouchUIDialogType.CORAL3.isOfType(getTouchUIDialogType())) {
-			return FixedColumnsLayoutElement.RESOURCE_TYPE_CORAL3;
-		}
-		return FixedColumnsLayoutElement.RESOURCE_TYPE;
-	}
+    private String path;
 
-	@Override
-	public void setResourceType(String resourceType) {
-		throw new UnsupportedOperationException("resource type is Static for Tabs Layout Element");
-	}
+    private TouchUIDialogElement renderCondition;
 
-	@Override
-	public String getFieldName() {
-		return this.fieldName;
-	}
+    private boolean showOnCreate;
 
-	@Override
-	public void setFieldName(String fieldName) {
-		this.fieldName = fieldName;
-	}
+    private boolean hideOnEdit;
 
-	public String getTitle() {
-		return title;
-	}
+    private String orderBefore;
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    @Override
+    public String getResourceType() {
+        if (StringUtils.isNotEmpty(path)) {
+            return Section.INCLUDE_RESOURCE_TYPE;
+        } else if (TouchUIDialogType.CORAL3.isOfType(getTouchUIDialogType())) {
+            return FixedColumnsLayoutElement.RESOURCE_TYPE_CORAL3;
+        }
+        return FixedColumnsLayoutElement.RESOURCE_TYPE;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    @Override
+    public void setResourceType(String resourceType) {
+        throw new UnsupportedOperationException("resource type is Static for Tabs Layout Element");
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    @Override
+    public String getFieldName() {
+        return this.fieldName == null ? super.getFieldName() : this.fieldName;
+    }
 
-	public TouchUIDialogElement getRenderCondition() {
-		return renderCondition;
-	}
+    @Override
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
 
-	public void setRenderCondition(TouchUIDialogElement renderCondition) {
-		this.renderCondition = renderCondition;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public boolean isShowOnCreate() {
-		return showOnCreate;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void setShowOnCreate(boolean showOnCreate) {
-		this.showOnCreate = showOnCreate;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public boolean isHideOnEdit() {
-		return hideOnEdit;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-	public void setHideOnEdit(boolean hideOnEdit) {
-		this.hideOnEdit = hideOnEdit;
-	}
+    public TouchUIDialogElement getRenderCondition() {
+        return renderCondition;
+    }
 
-	public String getOrderBefore() {
-		return orderBefore;
-	}
+    public void setRenderCondition(TouchUIDialogElement renderCondition) {
+        this.renderCondition = renderCondition;
+    }
 
-	public void setOrderBefore(String orderBefore) {
-		this.orderBefore = orderBefore;
-	}
+    public boolean isShowOnCreate() {
+        return showOnCreate;
+    }
+
+    public void setShowOnCreate(boolean showOnCreate) {
+        this.showOnCreate = showOnCreate;
+    }
+
+    public boolean isHideOnEdit() {
+        return hideOnEdit;
+    }
+
+    public void setHideOnEdit(boolean hideOnEdit) {
+        this.hideOnEdit = hideOnEdit;
+    }
+
+    public String getOrderBefore() {
+        return orderBefore;
+    }
+
+    public void setOrderBefore(String orderBefore) {
+        this.orderBefore = orderBefore;
+    }
 }

94c3f76f0f8a09dd97f94376869763059f2dc2dd
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-01-15 17:07:40 -0600

    comments.

--- cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/IncludeDialogFields.java
+++ cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/IncludeDialogFields.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Field annotation to include dialog fields on nested objects, i.e. to support composition of component classes as an
+ * alternative to inheritance.
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.FIELD)
 public @interface IncludeDialogFields {

5552cc1567a916a22fba6ad0f3713cf95e7bb6e5
Mark Daugherty <mark.r.daugherty@gmail.com>
2019-01-30 09:53:40 -0600

    added annotation property to RTE for fixed inline toolbar.

--- cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/widgets/RichTextEditor.java
+++ cq-component-annotations/src/main/java/com/citytechinc/cq/component/annotations/widgets/RichTextEditor.java
@@ -32,150 +32,135 @@ import java.lang.annotation.Target;
 public @interface RichTextEditor {
 
     /**
-     * Edit RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.EditToolsPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * Edit RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.EditToolsPlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return Edit[]
      */
     Edit[] edit() default {};
 
     /**
-     * FindReplace RTE Plugin configuration. For more information on configuring
-     * this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.FindReplacePlugin"
-     * >the RTE configuration documentation</a>.
+     * FindReplace RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.FindReplacePlugin" >the
+     * RTE configuration documentation</a>.
      *
      * @return FindReplace[]
      */
     FindReplace[] findreplace() default {};
 
     /**
-     * Format RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.FormatPlugin"
-     * >the RTE configuration documentation</a>.
+     * Format RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.FormatPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Format[]
      */
     Format[] format() default {};
 
     /**
-     * Image RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.ImagePlugin"
-     * >the RTE configuration documentation</a>.
+     * Image RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.ImagePlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Image[]
      */
     Image[] image() default {};
 
     /**
-     * Keys RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.KeyPlugin"
-     * >the RTE configuration documentation</a>.
+     * Keys RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.KeyPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Keys[]
      */
     Keys[] keys() default {};
 
     /**
-     * Justify RTE Plugin configuration. For more information on configuring
-     * this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.JustifyPlugin"
-     * >the RTE configuration documentation</a>.
+     * Justify RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.JustifyPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Justify[]
      */
     Justify[] justify() default {};
 
     /**
-     * Links RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.LinkPlugin"
-     * >the RTE configuration documentation</a>.
+     * Links RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.LinkPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Links[]
      */
     Links[] links() default {};
 
     /**
-     * Lists RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.ListPlugin"
-     * >the RTE configuration documentation</a>.
+     * Lists RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.ListPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return Lists
      */
     Lists[] lists() default {};
 
     /**
-     * MiscTools RTE Plugin configuration. For more information on configuring
-     * this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.MiscToolsPlugin"
-     * >the RTE configuration documentation</a>.
+     * MiscTools RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/index.html?class=CQ.form.rte.plugins.MiscToolsPlugin" >the RTE
+     * configuration documentation</a>.
      *
      * @return MiscTools[]
      */
     MiscTools[] misctools() default {};
 
     /**
-     * ParaFormat RTE Plugin configuration. For more information on configuring
-     * this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.ParagraphFormatPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * ParaFormat RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.ParagraphFormatPlugin.html" >the
+     * RTE configuration documentation</a>.
      *
      * @return ParaFormat[]
      */
     ParaFormat[] paraformat() default {};
 
     /**
-     * SpellCheck RTE Plugin configuration. For more information on configuring
-     * this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.SpellCheckerPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * SpellCheck RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.SpellCheckerPlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return SpellCheck[]
      */
     SpellCheck[] spellcheck() default {};
 
     /**
-     * Styles RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.StylesPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * Styles RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.StylesPlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return Styles[]
      */
     Style[] styles() default {};
 
     /**
-     * SubSuperscript RTE Plugin configuration. For more information on
-     * configuring this plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.SubSuperScriptPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * SubSuperscript RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.SubSuperScriptPlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return SubSuperscript[]
      */
     SubSuperscript[] subsuperscript() default {};
 
     /**
-     * Table RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.TablePlugin.html"
-     * >the RTE configuration documentation</a>.
+     * Table RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.TablePlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return Table[]
      */
     Table[] table() default {};
 
     /**
-     * Undo RTE Plugin configuration. For more information on configuring this
-     * plugin see <a href=
-     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.UndoRedoPlugin.html"
-     * >the RTE configuration documentation</a>.
+     * Undo RTE Plugin configuration. For more information on configuring this plugin see <a href=
+     * "http://dev.day.com/docs/en/cq/current/widgets-api/output/CQ.form.rte.plugins.UndoRedoPlugin.html" >the RTE
+     * configuration documentation</a>.
      *
      * @return Undo[]
      */
@@ -184,13 +169,20 @@ public @interface RichTextEditor {
     /**
      * For Touch-UI Only
      * <p>
-     * UI Settings Node configuration plugin see <a href=
-     * "https://docs.adobe.com/docs/en/aem/6-2/administer/operations/page-authoring/rich-text-editor.html?#Configuri
-     * n g RichText Editor dialog settings for toolbar (Touch UI)>Configuring
-     * RichText Editor dialog settings for toolbar (Touch UI)</a>.
+     * UI Settings Node configuration plugin see <a href= "https://docs.adobe.com/docs/en/aem/6-2/administer/operations/page-authoring/rich-text-editor.html?#Configuri
+     * n g RichText Editor dialog settings for toolbar (Touch UI)>Configuring RichText Editor dialog settings for
+     * toolbar (Touch UI)</a>.
      *
      * @return Undo[]
      */
     UISettings[] uiSettings() default {};
 
+    /**
+     * For Touch-UI Only
+     * <p>
+     * Use inline toolbar when rendering in edit dialog.
+     *
+     * @return true to use inline toolbar
+     */
+    boolean useFixedInlineToolbar() default true;
 }
